8730e55cefb141dd85e462578d5a9a9d221d344d
Alyan-khattak <alyankhattake@gmail.com>
2026-02-24 21:17:18 +0500

    Learned to create custom pkgs in python

---
+++ 3.Importing_Creating_Modules/2_test_Custom_pkg.py
@@ -0,0 +1,4 @@
+from My_Custom_package.Alyan_math import *
+
+print(add(3,4))
+print(prod(3,6))
---
+++ 3.Importing_Creating_Modules/My_Custom_package/Alyan_math.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+
+def prod(a,b):
+    return a*b
+
+# these function can be imported in other files
+# I Will import this in 1_import.ipynb file
