ffdf593d448fe288b322546362b9a9ca213af597
Alyan-khattak <alyankhattake@gmail.com>
2026-02-24 21:17:18 +0500

    Learned to create custom pkgs in python

---
+++ 3.Importing_Creating_Modules/My_Custom_package/__init__.py
@@ -0,0 +1,8 @@
+"""
+__init__.py is a special file used inside a folder to tell python
+-->> This Folder is a python package
+
+"""
+
+# When you want to create a custom package -->> Make sure to include init.py file in it ( it could be empty)
+# then make your own Modules inside that Package and can import it in another files ( Here I am making Alyan_Maths.py)
