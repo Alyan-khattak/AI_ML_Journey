690d245cd043c72d87e275b6b9ff447c2d1df270
Alyan-khattak <alyankhattake@gmail.com>
2026-02-26 22:16:42 +0500

    learned Paths and how to use them in file handling

--- 3.Importing_Creating_Modules/My_Custom_package/__init__.py
+++ 3.Importing_Creating_Modules/My_Custom_package/__init__.py
@@ -6,3 +6,6 @@ __init__.py is a special file used inside a folder to tell python
 
 # When you want to create a custom package -->> Make sure to include init.py file in it ( it could be empty)
 # then make your own Modules inside that Package and can import it in another files ( Here I am making Alyan_Maths.py)
+
+
+## WE can create a sub pkg by making a folder in this directory and follow the same method ( make init.py and ur modules)
